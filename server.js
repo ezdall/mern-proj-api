e61b6bb8f3362d4d5696b137ac8d1214768e8f9b
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-27 20:05:47 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const path = require('path');
+
+const { connectMDB } = require('./config/db');
+
+dotenv.config({
+  path: './config/config.env'
+});
+
+connectMDB().catch(err => console.error('connect-MongoDB Error', err.stack));
+
+const app = express();
+
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', express.static(path.join(__dirname, 'views')));
+
+app.use('/api', (req, res) => {
+  return res.json('hi world');
+});
+
+app.all('*', (req, res) => {
+  return res.json('error path');
+});
+
+// app.use()
+mongoose.connection.once('open', () => {
+  app.listen(PORT, err => {
+    if (err) throw err;
+
+    console.log(`MERN-Proj Server is running on http://localhost:${PORT}`);
+  });
+});
+
+mongoose.connection.on('error', err => {
+  console.error('error @mongoo-conn-error ---', err);
+});
+
+module.exports = app;

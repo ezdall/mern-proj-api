e477aa7359d4394e98a6aaf5339c6ab9f59552fe
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-28 14:08:37 +0800

    add cors and corsOptions

---
+++ config/cors-options.js
@@ -0,0 +1,21 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5000', // react
+
+  // no internet
+  'null'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+module.exports = { corsOptions };

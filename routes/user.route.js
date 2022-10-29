c81427c74d019494d76e0b80b69b95df332977c8
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-29 09:23:34 +0800

    add updateUser, deleteUser, use bcrypt

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,8 @@ const {
   userById,
   getAllUser,
   createUser,
+  updateUser,
+  deleteUser
 } = require('../controllers/user.cont');
 
 
@@ -14,6 +16,12 @@ router.get('/users', getAllUser)
 
 // POST
 router.post('/users', createUser)
+
+// PATCH
+router.patch('/users', updateUser)
+
+// DELETE
+router.delete('/users', deleteUser)
 
 // param
 router.param('userId', userById);

fd919f223e16467bbf8e3f9164d5eb0735be4df9
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-29 14:19:53 +0800

    add cookie-parser, login-limiter, 401 error

---
+++ middlewares/login-limiter.js
@@ -0,0 +1,18 @@
+const rateLimit = require('express-rate-limit');
+const { logEvents } = require('./logger')
+
+const loginLimiter = rateLimit({
+	windowsMs: 60 * 1000, // 1 min
+	max: 5, // limit each ip to 5 login request
+	message: {
+		message: 'Too many login attempt, try again in 60 sec'
+	},
+	standardHeaders: true, // return rate limit 'RateLimit-*'
+	legacyHeaders: false, // disable 'X-RateLimit-*'
+	handler(req, res, next, opts) {
+		logEvents(`Too many request: ${opts.message.message}\t${req.method}\t${req.url}\t${req.headers.origin || ''}`, 'errLog.log')
+		return res.status(opts.statusCode).json(opts.message)
+	}
+})
+
+module.exports = { loginLimiter }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-// const { loginLimiter } = require('../middlewares/login-limiter');
+const { loginLimiter } = require('../middlewares/login-limiter');
 const { login, logout, refresh } = require('../controllers/auth.cont')
 
-router.post('/auth', login)
+router.post('/auth', loginLimiter, login)
 
 router.get('/auth/refresh', refresh)
 
